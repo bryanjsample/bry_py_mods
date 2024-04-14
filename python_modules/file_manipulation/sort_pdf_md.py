83205c5e433ea5c8708858b3793cda8f527e57ea
Bryan Sample <bryanjsample@gmail.com>
2024-04-14 13:46:01 -0500

    Fixed sort_pdf_md.py after updates to parent classes

--- python_modules/file_manipulation/sort_pdf_md.py
+++ python_modules/file_manipulation/sort_pdf_md.py
@@ -1,6 +1,6 @@
 '''Class definition for working with multiple files as opposed to one.'''
 
-from directory import Directory
+from directory import Directory, get_key_press
 import os
 from time import sleep
 
@@ -12,30 +12,30 @@ class TargetFiles(Directory):
         destination = self.destination_path()
         if self.directory_does_not_exist():
             os.mkdir(destination)
-        for file in self.files:
-            os.rename(f'{self.path}/{file}', f'{destination}/{file}')
-        print(f'Moved all {self.target_extension} files.\n')
+        for file in self.Files:
+            os.rename(f'{self.Directory_Path}/{file}', f'{destination}/{file}')
+        print(f'Moved all {self.Target_Extension} files.\n')
         sleep(1)
 
     def directory_does_not_exist(self):
-        cwd_contents = os.listdir(self.path)
-        if f'{self.target_extension}_files' not in cwd_contents:
+        cwd_contents = os.listdir(self.Directory_Path)
+        if f'{self.Target_Extension}_files' not in cwd_contents:
             return True
         else:
             return False
     
     def destination_path(self):
-        return f'{self.path}/{self.target_extension}_files'
+        return f'{self.Directory_Path}/{self.Target_Extension}_files'
 
 def main():
     pdfs = TargetFiles('pdf')
     destination = pdfs.destination_path()
-    pdfs.get_key_press(f'\nPress enter to move files into {destination} or any other key to quit...\n')
+    get_key_press(message=f'\nPress enter to move files into {destination} or any other key to quit...\n')
     pdfs.sort_files()
 
     mds = TargetFiles('md')
     destination = mds.destination_path()
-    mds.get_key_press(f'\nPress enter to move files into {destination} or any other key to quit...\n')
+    get_key_press(message=f'\nPress enter to move files into {destination} or any other key to quit...\n')
     mds.sort_files()
 
 if __name__ == "__main__":
