b78089ee34837b7296662b0a8592f6644ad9a1ef
Bryan Sample <bryanjsample@gmail.com>
2024-04-14 13:54:15 -0500

    Added docstrings to sort_pdf_md.py

--- python_modules/file_manipulation/sort_pdf_md.py
+++ python_modules/file_manipulation/sort_pdf_md.py
@@ -1,14 +1,21 @@
-'''Class definition for working with multiple files as opposed to one.'''
+'''Module to sort files in a directory into their own sub-directories based on their extension'''
 
 from directory import Directory, get_key_press
 import os
 from time import sleep
 
-class TargetFiles(Directory):
+class Sorter(Directory):
+    '''
+        Class to sort files into their own sub-directories based on their extension.
+        All attributes are inherited from directory.Directory
+    '''
     def __init__(self, target_extension:str|list=False) -> None:
         Directory.__init__(self, target_extension)
 
-    def sort_files(self):
+    def sort_files(self) -> None:
+        '''
+            Move files into their respective sub-directories
+        '''
         destination = self.destination_path()
         if self.directory_does_not_exist():
             os.mkdir(destination)
@@ -17,26 +24,29 @@ class TargetFiles(Directory):
         print(f'Moved all {self.Target_Extension} files.\n')
         sleep(1)
 
-    def directory_does_not_exist(self):
+    def directory_does_not_exist(self) -> bool:
+        '''
+            If directories exist, great! If not, make them.
+        '''
         cwd_contents = os.listdir(self.Directory_Path)
         if f'{self.Target_Extension}_files' not in cwd_contents:
             return True
         else:
             return False
     
-    def destination_path(self):
+    def destination_path(self) -> str:
         return f'{self.Directory_Path}/{self.Target_Extension}_files'
 
 def main():
-    pdfs = TargetFiles('pdf')
+    mds = Sorter('md')
+    destination = mds.destination_path()
+    get_key_press(message=f'\nPress enter to move files into {destination} or any other key to quit...\n')
+    mds.sort_files()
+    pdfs = Sorter('pdf')
     destination = pdfs.destination_path()
     get_key_press(message=f'\nPress enter to move files into {destination} or any other key to quit...\n')
     pdfs.sort_files()
 
-    mds = TargetFiles('md')
-    destination = mds.destination_path()
-    get_key_press(message=f'\nPress enter to move files into {destination} or any other key to quit...\n')
-    mds.sort_files()
 
 if __name__ == "__main__":
     main()
