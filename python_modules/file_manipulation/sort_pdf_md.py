b9adbed49f6f0dad025682d7698501bbcc5364b5
Bryan Sample <bryanjsample@gmail.com>
2024-04-13 19:33:12 -0500

    Updated sort_pdf_md.py to include Target files, since the class is fairly specific to that use case.

--- python_modules/file_manipulation/sort_pdf_md.py
+++ python_modules/file_manipulation/sort_pdf_md.py
@@ -1,12 +1,41 @@
-from target_files import TargetFiles
+'''Class definition for working with multiple files as opposed to one.'''
+
+from directory import Directory
+import os
+from time import sleep
+
+class TargetFiles(Directory):
+    def __init__(self, target_extension:str|list=False) -> None:
+        Directory.__init__(self, target_extension)
+
+    def move_files(self):
+        destination = self.destination_path()
+        if self.directory_does_not_exist():
+            os.mkdir(destination)
+        for file in self.files:
+            os.rename(f'{self.path}/{file}', f'{destination}/{file}')
+        print(f'Moved all {self.target_extension} files.\n')
+        sleep(1)
+
+    def directory_does_not_exist(self):
+        cwd_contents = os.listdir(self.path)
+        if f'{self.target_extension}_files' not in cwd_contents:
+            return True
+        else:
+            return False
+    
+    def destination_path(self):
+        return f'{self.path}/{self.target_extension}_files'
 
 def main():
     pdfs = TargetFiles('pdf')
-    pdfs.get_key_press(f'\nPress enter to move files into {pdfs.destination} or any other key to quit...\n')
+    destination = pdfs.destination_path()
+    pdfs.get_key_press(f'\nPress enter to move files into {destination} or any other key to quit...\n')
     pdfs.move_files()
 
     mds = TargetFiles('md')
-    mds.get_key_press(f'\nPress enter to move files into {mds.destination} or any other key to quit...\n')
+    destination = mds.destination_path()
+    mds.get_key_press(f'\nPress enter to move files into {destination} or any other key to quit...\n')
     mds.move_files()
 
 if __name__ == "__main__":
