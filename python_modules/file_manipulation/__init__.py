0678bd036be5c00807f00797b4df5f87b4ee4ae7
Bryan Sample <bryanjsample@gmail.com>
2024-04-14 19:57:03 -0500

    minor

--- python_modules/file_manipulation/__init__.py
+++ python_modules/file_manipulation/__init__.py
@@ -1,4 +1,4 @@
-r'''
+'''
 Module to assist with selecting items from a directory.
     Author : Bryan Sample
     Date : 4/14/2024
@@ -11,7 +11,7 @@ Module to assist with selecting items from a directory.
             - Cairo : brew install cairo
             - Pandoc : brew install pandoc
         - Eisvogel Pandoc Template
-            - See 
+            - See README.md
 '''
 
 __all__ = ['directory', 'markdown_funcs', 'open_markup_file', 'sort_files']
