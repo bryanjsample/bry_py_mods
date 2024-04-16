fb2b3903afc51e1d9883fd00f400f1b5e939e3c3
Bryan Sample <bryanjsample@gmail.com>
2024-04-16 18:58:37 -0500

    added documentation to commands.py

--- brypymods/src/file_manipulation/commands.py
+++ brypymods/src/file_manipulation/commands.py
@@ -5,14 +5,23 @@ from file_manipulation.items_to_open import ItemsToOpen
 import sys
 
 def convert_md_to_pdf():
+    '''Looks for markdown files in the current directory, and allows you to choose one or more. Once items are selected, they will be converted to a pdf.'''
     markdown_files = MarkdownFiles()
     markdown_files.convert_files()
 
 def open_items():
+    '''
+        Will list all items in the current directory with the listed extensions. If no arguments are provided, then an option will
+        be given to show only unhidden items or all items.Once items are selected, you can choose to open them all at once or one at a time.
+    '''
     items = ItemsToOpen(sys.argv[1:])
     items.open_items()
 
 def sort_items():
+    '''
+        Will list all items in the current directory with the listed extensions. If no arguments are provided, then an option will be given to show
+        only unhidden items or all items.Once items are selected, they will be sorted into sub-directories based on their extension.
+    '''
     extensions = sys.argv[1:]
     if len(extensions) == 0:
         extensions = input('Enter a single-space seperated series of extensions to sort: ').split(' ')
